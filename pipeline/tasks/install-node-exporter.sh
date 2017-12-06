60b06378dc9809a4a55934b144b61e92c4e622ff
John Foley <jfoley@pivotal.io>
2017-12-06 12:21:26 -0700

    tickle

--- pipeline/tasks/install-node-exporter.sh
+++ pipeline/tasks/install-node-exporter.sh
@@ -6,32 +6,14 @@ TMPFILE=$(mktemp "$TMPDIR/runtime-config.XXXXXX")
 
 root_dir=$(cd "$(dirname "$0")/.." && pwd)
 
-export BOSH_ENVIRONMENT="127.0.0.1"
-export BOSH_CA_CERT=$(cat "pcf-bosh-creds/bosh-ca.pem")
-export BOSH_CLIENT=$(cat "pcf-bosh-creds/bosh-username")
-export BOSH_CLIENT_SECRET=$(cat "pcf-bosh-creds/bosh-pass")
-
 echo "Creating SSH tunnel"
 echo "$opsman_ssh_private_key" > opsman.key
-
-CURL="om --target https://${opsman_url} -k \
-  --username ${pcf_opsman_admin_username} \
-  --password ${pcf_opsman_admin_password} \
-  curl"
-export director_id=$($CURL --path=/api/v0/deployed/products | jq -r '.[] | select (.type == "p-bosh") | .guid')
-export director_ip=$($CURL --path=/api/v0/deployed/products/$director_id/static_ips | jq -r .[0].ips[0])
-
 chmod 0600 opsman.key
-ssh -oStrictHostKeyChecking=no -N \
-    ${opsman_ssh_user}@${opsman_url} \
-    -i opsman.key \
-    -L 25555:${director_ip}:25555 \
-    -L 4222:${director_ip}:4222 \
-    -L 25250:${director_ip}:25250 \
-    -L 25777:${director_ip}:25777 \
-    &
-
+ssh -4 -D 5000 -NC ${opsman_ssh_user}@${opsman_url} -i opsman.key &
 echo $! > ssh-tunnel.pid
+
+export BOSH_ALL_PROXY=socks5://localhost:5000
+login_to_director pcf-bosh-creds
 
 echo "Uploading Node exporter Release..."
 bosh2 upload-release https://bosh.io/d/github.com/cloudfoundry-community/node-exporter-boshrelease --sha1 a0018f96dd78525cae3687cfa1d9353aac7a0e02
