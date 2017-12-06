22c128de98f3bf0ac78a3eee80ef232f4bfa9fc4
John Foley <jfoley@pivotal.io>
2017-12-06 14:54:31 -0700

    yarp

--- pipeline/tasks/upload-prometheus-release.sh
+++ pipeline/tasks/upload-prometheus-release.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -ex
 
-export BOSH_CA_CERT=$external_bosh_ca_cert
-export BOSH_ENVIRONMENT=$external_bosh_address
-export BOSH_CLIENT=$external_bosh_client
-export BOSH_CLIENT_SECRET=$external_bosh_client_secret
+echo "$opsman_ssh_private_key" > opsman.key
+chmod 0600 opsman.key
+ssh -oStrictHostKeyChecking=no \
+    -4 -D 5000 -NC \
+    ${opsman_ssh_user}@${opsman_url} \
+    -i opsman.key &
+
+echo $! > ssh-tunnel.pid
+
+export BOSH_ALL_PROXY=socks5://localhost:5000
+login_to_director pcf-bosh-creds
 
 echo "Uploading prometheus Release..."
 bosh2 upload-release https://bosh.io/d/github.com/cloudfoundry-community/prometheus-boshrelease?v=20.0.0 --sha1 11a18875879a7712c67e6d30c4a069101ae77560
 bosh2 upload-release https://bosh.io/d/github.com/cloudfoundry/postgres-release?v=21 --sha1 b37916b709fb45ac7de3e195456301cb01a1ae22
+
+kill $(cat ssh-tunnel.pid)
